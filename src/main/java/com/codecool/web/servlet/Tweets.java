d39c0acfd46560d9ce70197d49447a06b4ecac23
adamKonyari <adam.konyari@gmail.com>
2019-04-17 13:50:03 +0200

    xml service ok

---
+++ src/main/java/com/codecool/web/servlet/Tweets.java
@@ -0,0 +1,29 @@
+package com.codecool.web.servlet;
+
+import com.codecool.web.model.Tweet;
+import com.codecool.web.service.TweetService;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet({"/tweets"})
+public class Tweets extends HttpServlet {
+
+    private TweetService tweetService = new TweetService();;
+    private List<Tweet> tweetList = new ArrayList<>();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        tweetList = tweetService.getTweetList();
+        req.setAttribute("tweetList", tweetList);
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher("tweets.jsp");
+        requestDispatcher.forward(req, resp);
+    }
+}
